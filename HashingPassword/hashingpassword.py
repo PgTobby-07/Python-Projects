6dc5ea792bd1044bb4fbe2b5e3ccdc31dcd5cd35
Praise Tobby <praisegodtobby@gmail.com>
2026-04-20 08:47:31 +0300

    Initial commit

---
+++ HashingPassword/hashingpassword.py
@@ -0,0 +1,22 @@
+#running format:
+
+# python hashingpassword.py <password> -t <hash-type>
+
+import argparse
+import hashlib
+
+# parsing
+parser = argparse.ArgumentParser(description='hashing given password')
+parser.add_argument('password', help='input password you want to hash')
+parser.add_argument('-t', '--type', default='sha256',choices=['sha256', 'sha512', 'md5'] )
+args = parser.parse_args() 
+
+# hashing given password
+password = args.password
+hashtype = args.type
+m = getattr(hashlib,hashtype)()
+m.update(password.encode())
+
+# output
+print(f"< hash-type : {hashtype} >")
+print(m.hexdigest())
